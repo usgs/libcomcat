a82eadf5dddc01096d7c6b4e8b43b8b32cc4aac7
Michael Hearne <mhearne@usgs.gov>
2013-07-24 15:52:07 -0600

    Fixed setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='libcomcat',
+      version='0.1dev',
+      description='USGS ComCat search API in Python',
+      author='Mike Hearne',
+      author_email='mhearne@usgs.gov',
+      url='',
+      packages=['libcomcat'],
+      scripts = ['getcomcat.py','getcsv.py'],
+)
