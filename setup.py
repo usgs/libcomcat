c0b33ab5315ce1087c1124826d9e800e40711579
Michael Hearne <mhearne@usgs.gov>
2015-04-13 14:01:52 -0600

    Added findid to script list

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='libcomcat',
       author_email='mhearne@usgs.gov',
       url='',
       packages=['libcomcat'],
-      scripts = ['getcomcat.py','getcsv.py','getfixed.py','getellipse.py'],
+      scripts = ['getcomcat.py','getcsv.py','getfixed.py','getellipse.py','findid.py'],
 )
