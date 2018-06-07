5c2da5acaa9d9d9b30c00ff4783c6402bc1dc387
emthompson@usgs.gov <emthompson@usgs.gov>
2018-06-07 14:12:28 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -2,10 +2,24 @@
 
 unamestr=`uname`
 if [ "$unamestr" == 'Linux' ]; then
-    source ~/.bashrc
+    prof=~/.bashrc
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    matplotlibdir=~/.config/matplotlib
+    env_file=environment_linux.yml
 elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
-    source ~/.bash_profile
+    prof=~/.bash_profile
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    matplotlibdir=~/.matplotlib
+    env_file=environment_osx.yml
+else
+    echo "Unsupported environment. Exiting."
+    exit
 fi
+
+source $prof
+
+echo "Path:"
+echo $PATH
 
 VENV=comcat
 
@@ -15,27 +29,56 @@ while getopts r FLAG; do
   case $FLAG in
     r)
         reset=1
-        echo "Letting conda sort out dependencies..."
+        
       ;;
   esac
 done
 
+
+# create a matplotlibrc file with the non-interactive backend "Agg" in it.
+if [ ! -d "$matplotlibdir" ]; then
+    mkdir -p $matplotlibdir
+fi
+matplotlibrc=$matplotlibdir/matplotlibrc
+if [ ! -e "$matplotlibrc" ]; then
+    echo "backend : Agg" > "$matplotlibrc"
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+elif grep -Fxq "backend : Agg" $matplotlibrc ; then
+    :
+elif [ ! grep -Fxq "backend" $matplotlibrc ]; then
+    echo "backend : Agg" >> $matplotlibrc
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+else
+    sed -i '' 's/backend.*/backend : Agg/' $matplotlibrc
+    echo "###############"
+    echo "NOTE: $matplotlibrc has been changed to set 'backend : Agg'"
+    echo "###############"
+fi
+
+
 # Is conda installed?
 conda --version
 if [ $? -ne 0 ]; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh;
+    echo "No conda detected, installing miniconda..."
+
+    curl $mini_conda_url -o miniconda.sh;
+    echo "Install directory: $HOME/miniconda"
+
     bash miniconda.sh -f -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
+
+    # Need this to get conda into path
+    . $HOME/miniconda/etc/profile.d/conda.sh
+else
+    echo "conda detected, installing $VENV environment..."
 fi
 
+echo "PATH:"
+echo $PATH
+echo ""
+
+
 # Choose an environment file based on platform
-unamestr=`uname`
-if [ "$unamestr" == 'Linux' ]; then
-    env_file=environment_linux.yml
-elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
-    env_file=environment_osx.yml
-fi
+echo ". $HOME/miniconda/etc/profile.d/conda.sh" >> $prof
 
 # If the user has specified the -r (reset) flag, then create an
 # environment based on only the named dependencies, without
@@ -45,15 +88,9 @@ if [ $reset == 1 ]; then
     env_file=environment.yml
 fi
 
-echo "Environment file: $env_file"
-
-# Turn off whatever other virtual environment user might be in
-conda deactivate
-
-# Download dependencies not in conda or pypi
-curl --max-time 60 --retry 3 -L \
-    https://github.com/usgs/earthquake-impact-utils/archive/0.7.zip -o impact-utils.zip
-
+# Start in conda base environment
+echo "Activate base virtual environment"
+conda activate base
 
 # Create a conda virtual environment
 echo "Creating the $VENV virtual environment:"
@@ -63,21 +100,20 @@ conda env create -f $env_file --force
 # Clean up zip files we've downloaded
 if [ $? -ne 0 ]; then
     echo "Failed to create conda environment.  Resolve any conflicts, then try again."
-    echo "Cleaning up zip files..."
-    rm impact-utils.zip
     exit
 fi
+
 
 # Activate the new environment
 echo "Activating the $VENV virtual environment"
 conda activate $VENV
 
-# Clean up downloaded packages
-rm impact-utils.zip
-
 # This package
-echo "Installing ${VENV}..."
+echo "Installing libcomcat..."
 pip install -e .
 
-#tell the user they have to activate this environment
-echo "Type 'conda activate ${VENV}' to use this new virtual environment."
+# Install default profile
+#python bin/sm_profile -c default -a
+
+# Tell the user they have to activate this environment
+echo "Type 'conda activate $VENV' to use this new virtual environment."
